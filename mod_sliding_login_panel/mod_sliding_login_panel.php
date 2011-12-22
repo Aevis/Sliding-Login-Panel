eac4550b8c605fffe4eb65ca71a33fc4e2aa1665
Silas Hellinger <silas.hellinger@gmail.com>
2011-12-22 16:50:13 +0100

    media files reorg, css + param added

---
+++ mod_sliding_login_panel/mod_sliding_login_panel.php
@@ -0,0 +1,6 @@
+<?php
+// no direct access
+defined('_JEXEC') or die;
+
+// include the template for display
+require JModuleHelper::getLayoutPath('mod_sliding_login_panel', $params->get('layout', 'default'));
---
+++ mod_sliding_login_panel/tmpl/default.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ *
+ * @version	$Id$
+ * @package	Sliding Login Panel
+ * @copyright	Copyright (C) 2011 Silas Hellinger https://github.com/Aevis. All rights reserved.
+ * @license	GNU/GPLv2
+ */
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+$document = &JFactory::getDocument();
+$document->addScript('https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+$document->addScript( JURI::root().'/media/mod_sliding_login_panel/js/slide.js' );
+$document->addStyleSheet( JURI::root()."/media/mod_sliding_login_panel/css/slide.css");
+$document->addStyleSheet( JURI::root()."/media/mod_sliding_login_panel/css/style.css");
+?>
+
+<div id="toppanel">
+	<div id="panel">
+            <div class="content clearfix">
+                <div class="left">		
+                    <?php
+                        jimport('joomla.application.module.helper');
+                        $modules = JModuleHelper::getModules('slidingpanel');
+                        foreach($modules as $module)
+                        {
+                            echo JModuleHelper::renderModule($module, array('style'=>'xhtml'));
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+	<div class="tab">
+            <ul class="login">
+                <li class="left">&nbsp;</li>
+                <li>Hello Guest!</li>
+                <li class="sep">|</li>
+                <li id="toggle">
+                    <a id="open" class="open" href="#">Log In | Register</a>
+                    <a id="close" style="display: none;" class="close" href="#">Close Panel</a>			
+                </li>
+                <li class="right">&nbsp;</li>
+            </ul> 
+	</div>
+</div>
